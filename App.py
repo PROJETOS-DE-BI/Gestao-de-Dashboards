397b68518c1c4ee48ea657fdf5b8c9669a105e75
Gustavo <gustavo.rodrigues@coreplan.com.br>
2024-08-17 17:50:58 -0300

    Mudança de nome do arquivo

--- App.py
+++ App.py
@@ -5,6 +5,14 @@ import streamlit as st
 
 st.set_page_config(page_title="JGR - Gestão de Dashboards")
 
-st.subheader("Meu primeiro site com Streamlit")
-st.title("Dashboard de Contratos")
-st.write("Informações sobre os contratos fechados pela JGR dos meses de 2024")
+with st.container():
+
+    st.subheader("Meu primeiro site com Streamlit")
+    st.title("Dashboard de Contratos")
+    st.write("Informações sobre os contratos fechados pela JGR dos meses de 2024")
+
+with st.container():
+    st.write("---")
+    dados = pd.read_csv("contrato.csv")
+    # Gera o gráfico de área
+    st.area_chart(dados, x="data", y="qtde_h")
