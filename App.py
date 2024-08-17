5f7655262d63b32fdc33798b79173d6e66cd6f1c
Gustavo <gustavo.rodrigues@coreplan.com.br>
2024-08-17 15:25:54 -0300

    Arquivo de dados

--- App.py
+++ App.py
@@ -5,4 +5,6 @@ import streamlit as st
 
 st.set_page_config(page_title="JGR - Gestão de Dashboards")
 
-st.write("Meu primeiro site com Streamlit")
+st.subheader("Meu primeiro site com Streamlit")
+st.title("Dashboard de Contratos")
+st.write("Informações sobre os contratos fechados pela JGR dos meses de 2024")
